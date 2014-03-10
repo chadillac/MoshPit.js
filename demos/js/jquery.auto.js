9136e4b7ba00f3f148a19762a54078e333f3d67b
Chad Seaman <chadillac83@gmail.com>
2014-03-09 22:36:15 -0700

    Update demos to have better annotated source via docco

--- demos/js/jquery.auto.js
+++ demos/js/jquery.auto.js
@@ -1,15 +1,25 @@
 (function() {
+    // setup our private `gui` object to storing our Panels
+    // and hooking up our listeners and functionality
     var gui = {
+        // where we'll store our objects
         elems:{},
+        // our init process that we'll trigger on `$.ready`
         init: function() {
+            // store our references
             this.elems.$Panel_1 = $('#Panel_1').html('#Panel_1');    
-            this.elems.$Panel_2 = $('#Panel_2').html('#Panel_2');    
+            this.elems.$Panel_2 = $('#Panel_2').html('#Panel_2');
+
+            // *note: using `MoshPit.join` will catch 
+            // `$.show`,`$.hide`, and `$.toggle` 
+            // calls to jQuery and update our views shown states accordingly*
+            MoshPit.join(this.elems.$Panel_1);
+            MoshPit.join(this.elems.$Panel_2);
         }    
     };    
 
+    // on `$.ready` we'll init our `gui`
     $(function(){
         gui.init();
-        MoshPit.join(gui.elems.$Panel_1);
-        MoshPit.join(gui.elems.$Panel_2);
     });
 })();
--- demos/js/vanilla.js
+++ demos/js/vanilla.js
@@ -1,20 +1,33 @@
+// ** setup on window.onload **  
 window.onload = function() {
+    // Track #Panel_1 & #Panel_2 with `MoshPit.js`
     MoshPit.join('Panel_1');
     MoshPit.join('Panel_2');
 };
 
+// ** show_a_panel **  
+// show one of our Panels
 function show_a_panel(which) {
+    // use jQuery `.show` to make a Panel visible
     $('#Panel_'+which).show();    
 }
 
+// ** hide_a_panel **  
+// hide one of the Panels
 function hide_a_panel(which) {
+    // use `MoshPit.hide` to make a Panel hidden
     MoshPit.hide('Panel_'+which);    
 }
 
+// ** toggle_a_panel **  
+// hide or show a panel based on it's current shown state 
 function toggle_a_panel(which) {
     var panel = 'Panel_'+which;
+    // see if a Panel is currently visible
     var shown = MoshPit.is_shown(panel);
 
+    // if it's visible, we'll use `hide_a_panel` to hide it
+    // if it's not we'll show it with `show_a_panel`
     if (shown === true) {
         hide_a_panel(which); 
     } else {
@@ -22,7 +35,10 @@ function toggle_a_panel(which) {
     }
 }
 
+// ** toggle_state **  
+// add or remove a state based on it currently being set or not
 function toggle_state() {
+    // use `MoshPit.toggle_state` to enabled the `horz` state
     MoshPit.toggle_state('horz');
 }
 
