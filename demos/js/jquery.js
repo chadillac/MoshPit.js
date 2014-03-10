80b11db01edbefc8cccde57add810824a5c1c537
Chad Seaman <chadillac83@gmail.com>
2014-03-09 22:37:59 -0700

    Updated demos, docco generated source for them

--- demos/js/jquery.js
+++ demos/js/jquery.js
@@ -1,10 +1,19 @@
 (function() {
+    // setup our private `gui` object to storing our Panels
+    // and hooking up our listeners and functionality
     var gui = {
         elems:{},
         init: function() {
+            // store our #Panel_1 and #Panel_2
             this.elems.$Panel_1 = $('#Panel_1').html('#Panel_1');    
             this.elems.$Panel_2 = $('#Panel_2').html('#Panel_2');    
 
+            // listen for clicks on elements we care about
+            // and use the global methods to interaction manually
+            // with `MoshPit`.  
+            // *note: if you're not tracking views with `MoshPit.join` 
+            // calls to `$.show`,`$.hide`,`$.toggle` will not notify
+            // `MoshPit.js` of changes in the view state.
             $('body').on('click','.Panel_1_toggle',function(){
                 MoshPit.toggle(gui.elems.$Panel_1);
             });
@@ -14,9 +23,11 @@
             $('body').on('click','.Panels_toggle',function(){
                 MoshPit.toggle('Panel_1,Panel_2');    
             });
+            //
         }    
     };    
 
+    // on `$.ready` `init` our `gui`  
     $(function(){
         gui.init();     
     });
--- demos/js/marionette.js
+++ demos/js/marionette.js
@@ -1,5 +1,7 @@
+// create our Demo Marionette application
 Demo = new Backbone.Marionette.Application();
 
+// sreate our `PanelView` class that we'll use in our Regions
 PanelView = Backbone.Marionette.ItemView.extend({
     initialize: function(options) {
         self = this;
@@ -10,21 +12,24 @@ PanelView = Backbone.Marionette.ItemView.extend({
     }
 });
 
+// sreate our `PanelRegion` that we'll use in our Application
 PanelRegion = Backbone.Marionette.Region.extend({
     initialize: function(options){
         this.el = options.el;
+        // when we initialize our `PanelRegion` we'll
+        // tell `MoshPit.js` to track it.
         MoshPit.join(this);
     }
 });
 
+// on `$.ready` hook everything up
 $(function(){
+    // create our new `PanelRegion`s that target #Panel_1 & #Panel_2
     Demo.Panel_1 = new PanelRegion({el:"#Panel_1"});
     Demo.Panel_2 = new PanelRegion({el:"#Panel_2"});
+    // load our Regions up with `PanelView`s
     Demo.Panel_1.show(new PanelView({name:"Panel 1"}));
     Demo.Panel_2.show(new PanelView({name:"Panel 2"}));
+    // Start our application up
     Demo.start();
 });
-
-function remove() {
-    MoshPit.leave(Demo.Panel_1);    
-}
